f0708df38cb156f05bfe11e3ce3e20f9f890ad01
woong <koohw0109@gmail.com>
2023-08-29 11:48:34 +0900

    test

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -79,33 +79,46 @@ class _HomePageState extends State<HomePage> {
                     padding: const EdgeInsets.all(20),
                     width: 1290.w,
                     height: 500.h,
-                    child: Stack(
-                      children: [
-                        // Positioned(
-                        //   left: 0,
-                        //   top: 0,
-                        //   child: TemplateBox(
-                        //     width: 340.w,
-                        //     height: 230.h,
-                        //     templateTitle: "welcome",
-                        //     backgroundColor: const Color(0xFFfff3da),
-                        //   ),
-                        // ),
-                        Positioned(
-                          left: boxPosition.dx,
-                          top: boxPosition.dy,
-                          child: Draggable(
-                            feedback: const MemoBox(),
-                            onDraggableCanceled: (velocity, offset) {
-                              setState(() {
-                                boxPosition = offset;
-                              });
-                            },
-                            child: const MemoBox(),
-                          ),
-                        ),
-                      ],
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 10, // Number of columns in the grid
+                        mainAxisSpacing: 20, // Spacing between rows
+                        crossAxisSpacing: 20, // Spacing between columns
+                      ),
+                      itemCount:
+                          30, // Number of MemoBox widgets you want to display
+                      itemBuilder: (context, index) {
+                        return const MemoBox();
+                      },
                     ),
+                    // Stack(
+                    //   children: [
+                    //     // Positioned(
+                    //     //   left: 0,
+                    //     //   top: 0,
+                    //     //   child: TemplateBox(
+                    //     //     width: 340.w,
+                    //     //     height: 230.h,
+                    //     //     templateTitle: "welcome",
+                    //     //     backgroundColor: const Color(0xFFfff3da),
+                    //     //   ),
+                    //     // ),
+                    //     Positioned(
+                    //       left: boxPosition.dx,
+                    //       top: boxPosition.dy,
+                    //       child: Draggable(
+                    //         feedback: const MemoBox(),
+                    //         onDraggableCanceled: (velocity, offset) {
+                    //           setState(() {
+                    //             boxPosition = offset;
+                    //           });
+                    //         },
+                    //         child: const MemoBox(),
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
                   ),
                   const ScrollBar(),
                 ],
